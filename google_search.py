bd6457f4b29e6dfdac49c6432b2d5817b09c11ca
KingsManhub <ayodammys@gmail.com>
2022-11-07 09:39:34 -0500

    we make redirected the output to a file

--- google_search.py
+++ google_search.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 
+
 try:
     from googlesearch import search
 except ImportError:
     print("No module named 'google' found")
  
 # to search
-query = input("Search Web Page: ")
-num_of_result = int(input("Enter number of result: "))
+Query = input("Search Web Page: ")
+num_of_result = int(input("Enter number of result:"))
  
-for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-    print(j)
+title = ""
+for i in Query:
+    if i == " ":
+        i = "_"
+    title = title + i
+title = title+".txt"
+for j in search(Query, tld="co.in", num=10, stop=10, pause=2):
+    with open (title, "a") as inside:
+        inside.writelines(str(j)+"\n")
+        
