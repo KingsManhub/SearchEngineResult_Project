da1e93ef8979d91b2201c50782de39dd74c333e7
KingsManhub <ayodammys@gmail.com>
2022-11-14 05:29:43 +0100

    pushing 1

--- google_search.py
+++ google_search.py
@@ -3,6 +3,7 @@ from IPython.display import HTML
 import re
 import urllib.request
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 
 
 """____________This section returns the urls_______________"""
@@ -15,27 +16,46 @@ except ImportError:
 Query = input("Search Web Page: ")
 num_of_result = int(input("Enter number of result:"))
  
-title = ""
+link_file_name = ""
 for i in Query:
-    if i == " ":
-        i = "_"
-    title = title + i
-title = title+".txt"
-for j in search(Query, tld="co.in", num=num_of_result, stop=num_of_result, pause=2):
-    with open (title, "a") as inside:
-        inside.writelines(str(j)+"\n")
+    link_file_name += i.replace(" ", "_")
+link_file_name +=".txt"
 
+def file_of_links(Link):
+    with open (link_file_name, "a") as inside:
+        inside.writelines(str(Link)+"\n")
+        
+searcher = search(Query, tld="co.in", num=num_of_result, stop=num_of_result, pause=2)
+    
+with ThreadPoolExecutor(max_workers=num_of_result) as executor:
+    executor.map(file_of_links, searcher)
 
 
 """____________This section is for ranking___________ """
 keywords = Query.split(" ")
-print(keywords)
-def order_of_relevance(title, num):
-    with open(title, "r") as Read:
-        for line in Read:
-            r = urllib.request.urlopen(line).read()
-            soup = BeautifulSoup(r, "lxml")
-            with open("stripped_"+title, "w") as w:
-                w.write(soup.get_text())
 
-order_of_relevance(title, num_of_result)
+count = 0
+with open(link_file_name, "r") as Read:
+    for eachLink in Read:
+        with open("plain_"+link_file_name, "a") as Write:
+            Write.write("stripped_"+str(count)+link_file_name+"\n")
+
+        r = urllib.request.urlopen(eachLink.strip()).read()
+        soup = BeautifulSoup(r, "lxml")
+        with open("stripped_"+str(count)+link_file_name, "w") as w:
+            w.write(soup.get_text())
+        count += 1
+    
+with open(link_file_name, "r") as Read:
+    list_link = Read.readlines()
+for i in range(0, len(list_link)):
+    with open("stripped_"+str(i)+link_file_name, "r") as plain:
+        whole_content = plain.read()
+        summary = ""
+        for j in range(len(whole_content)//1.5):
+            if j == " " or i == "\n":
+                j = ""
+            summary += whole_content[j]
+        with open("summary.txt", "a") as s:
+            s.write(f"{i+1}. {list_link[i].strip()}:")
+            s.write("\n\t"+summary+"...\n\n\n")
